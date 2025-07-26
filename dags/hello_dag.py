074714558dc0ed3f052cf22fc735318b61668efc
larakrdnz03 <laraminakaradenizz@gmail.com>
2025-07-26 21:39:32 +0300

    Create hello_dag.py

---
+++ dags/hello_dag.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def say_hello():
+    print("Merhaba Astro!")
+
+with DAG(
+    dag_id="hello_test_dag",
+    start_date=datetime(2025, 7, 1),
+    schedule_interval="@hourly",
+    catchup=False
+) as dag:
+    task = PythonOperator(
+        task_id="hello_task",
+        python_callable=say_hello
+    )
